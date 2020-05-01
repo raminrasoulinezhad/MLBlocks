45a8eb3107ddadede02334c03ef0164cb1ee21fb
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-05-02 01:07:06 +1000

    modify

--- verilog/accumulator.v
+++ verilog/accumulator.v
@@ -23,7 +23,7 @@ module accumulator (
 	parameter WIDTH = 32;
 	parameter DEPTH = 1;
 
-	localparam DEPTH_CNTL = (DEPTH > 1)? (DEPTH-1): 1; 
+	localparam DEPTH_CNTL = (DEPTH == 1) ? 1 : (DEPTH-1); 
 
 	///////// IOs
 	input clk;
@@ -52,37 +52,35 @@ module accumulator (
 	assign res_in = (res_in_select == 1'b1) ? res_in_v : res_in_h;
 
 	generate  
-
 		if (TYPE == "FEEDBACK") begin 
 			assign acc_in = (acc_mode == 1'b0) ? res_in : acc[0];
 		end else begin 
 			assign acc_in = res_in;
 		end 
-
-		integer i;
-		always @(posedge clk)begin
-			if (reset) begin
-				for (i = 0; i < DEPTH; i = i + 1) begin 
-					acc[i] <= 0;
-				end 
-			end else if (acc_en) begin
-				acc[0] <= acc_in + shifter_res;
-
-				for (i = 1; i < DEPTH; i = i + 1) begin 
-					acc[i] <= acc_temp[i-1];
-				end 
-			end 
-		end
-		
-		integer j;
-		always @ (*)begin
-			acc_temp[0] = acc[0];
-			for (j = 1; j < DEPTH; j = j + 1) begin 
-				acc_temp[j] = (acc_depth[j-1]) ? acc[j] : acc_temp[j-1];
-			end
-		end 
-
 	endgenerate
+
+	integer i;
+	always @(posedge clk)begin
+		if (reset) begin
+			for (i = 0; i < DEPTH; i = i + 1) begin 
+				acc[i] <= 0;
+			end 
+		end else if (acc_en) begin
+			acc[0] <= acc_in + shifter_res;
+
+			for (i = 1; i < DEPTH; i = i + 1) begin 
+				acc[i] <= acc_temp[i-1];
+			end 
+		end 
+	end
+	
+	integer j;
+	always @ (*)begin
+		acc_temp[0] = acc[0];
+		for (j = 1; j < DEPTH; j = j + 1) begin 
+			acc_temp[j] = (acc_depth[j-1]) ? acc[j] : acc_temp[j-1];
+		end
+	end 
 
 	assign res_out_h = acc_temp[DEPTH-1];
 	assign res_out_v = acc_temp[DEPTH-1];
