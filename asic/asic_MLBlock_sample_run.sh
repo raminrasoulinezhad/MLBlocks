6b438478cc7add18c2edd716e75d12159a08b438
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-05-01 13:32:01 +1000

    update

--- asic/asic_MLBlock_sample_run.sh
+++ asic/asic_MLBlock_sample_run.sh
@@ -4,7 +4,7 @@ mkdir -p MLBlock_sample_dir
 mkdir -p MLBlock_sample_dir/files
 cp -r ../verilog/*.v ./MLBlock_sample_dir/files
 
-NUM_CORES=2
+NUM_CORES=10
 
 parallel --bar --gnu -j${NUM_CORES} --header : \
     'bash ./asic_MLBlock_sample_exp.sh {PE_W} {PE_H} {TYPE} {REUSE}' \
