c3aa320d2c3817a65439d4d69fe060318ba05259
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-09-08 01:45:01 +1000

    update

--- Alg2Block/benchmark_archs.py
+++ Alg2Block/benchmark_archs.py
@@ -56,6 +56,78 @@ arch_6 = Arch("all",
 			)
 
 
+arch_12_16x16 = Arch("all", 
+			None, 
+			my_space,
+			stationary="W",
+			precisions={	
+							"I" : 8,
+							"W" : 8,
+							"O" : 32,
+						},
+			subprecisions=	{	
+						"I_D" : 4,
+						"W_D" : 2,
+						"RES_D" : 1,
+						"SHIFTER_TYPE" : "BYPASS",	# "BYPASS", "2Wx2V_by_WxV", "2Wx2V_by_WxV_apx", "2Wx2V_by_WxV_apx_adv"
+					},
+			nmac=12,
+			limits= {	
+						"IO_I" : 36,
+						"IO_W" : None,
+						"IO_O" : 32*4,
+						"IO" : None, 	# 48+30+18+27+48 # = DSP48_out + DSP48_A + DSP48_B + DSP48_C + DSP48_D
+					}
+			)
+
+arch_9_16x16 = Arch("all", 
+			None, 
+			my_space,
+			stationary="W",
+			precisions={	
+							"I" : 8,
+							"W" : 8,
+							"O" : 32,
+						},
+			subprecisions=	{	
+						"I_D" : 4,
+						"W_D" : 2,
+						"RES_D" : 1,
+						"SHIFTER_TYPE" : "BYPASS",	# "BYPASS", "2Wx2V_by_WxV", "2Wx2V_by_WxV_apx", "2Wx2V_by_WxV_apx_adv"
+					},
+			nmac=9,
+			limits= {	
+						"IO_I" : 36,
+						"IO_W" : None,
+						"IO_O" : 32*4,
+						"IO" : None, 	# 48+30+18+27+48 # = DSP48_out + DSP48_A + DSP48_B + DSP48_C + DSP48_D
+					}
+			)
+
+arch_6_16x16 = Arch("all", 
+			None, 
+			my_space,
+			stationary="W",
+			precisions={	
+							"I" : 8,
+							"W" : 8,
+							"O" : 32,
+						},
+			subprecisions=	{	
+						"I_D" : 4,
+						"W_D" : 2,
+						"RES_D" : 1,
+						"SHIFTER_TYPE" : "BYPASS",	# "BYPASS", "2Wx2V_by_WxV", "2Wx2V_by_WxV_apx", "2Wx2V_by_WxV_apx_adv"
+					},
+			nmac=6,
+			limits= {	
+						"IO_I" : 36,
+						"IO_W" : None,
+						"IO_O" : 32*4,
+						"IO" : None, 	# 48+30+18+27+48 # = DSP48_out + DSP48_A + DSP48_B + DSP48_C + DSP48_D
+					}
+			)
+
 print ("\n-- Arch. descriptions are loaded\n")
 
 if __name__ == "__main__":
---
+++ Alg2Block/script_table_III.py
@@ -0,0 +1,29 @@
+import os
+
+from Param import Param
+from Space import Space
+from Arch import Arch
+from Algorithm import Algorithm
+from Unrolling import Unrolling
+from ImpConfig import ImpConfig 
+
+from benchmark_space import * 
+from benchmark_algs import * 
+from benchmark_archs import * 
+
+from tabulate import get_asic_results
+
+print ('lets do synthesis for DSP48 which takes time. Please, be patient :) ')
+os.system('cd ../asic && make clean && make asic_dsp  >/dev/null 2>&1')
+area, freq, power = get_asic_results('../asic/', period=1333)
+print ('area: %f\tfreq: %f\tpower: %f' % (area, freq, power))
+
+# heuristic approach
+arch_12_16x16.search_heuristic_v2(baidu_algs, MLblock_version='v2', period=1333, verbose=False)
+arch_9_16x16.search_heuristic_v2 (baidu_algs, MLblock_version='v2', period=1333, verbose=False)
+arch_6_16x16.search_heuristic_v2 (baidu_algs, MLblock_version='v2', period=1333, verbose=False)
+
+#arch_12_16x16.search_area_in_loop(baidu_algs, subset_length=2, do_gen_hdl=True, do_synthesis=True, period=1333, MLblock_version='v2', objective='obj')
+#arch_9_16x16.search_area_in_loop (baidu_algs, subset_length=2, do_gen_hdl=True, do_synthesis=True, period=1333, MLblock_version='v2', objective='obj')
+#arch_6_16x16.search_area_in_loop (baidu_algs, subset_length=2, do_gen_hdl=True, do_synthesis=True, period=1333, MLblock_version='v2', objective='obj')
+
