4a88b3f9d148ebaddcc41fedea0af87c6593c698
raminrasoulinezhad <ramin_r348@yahoo.com>
2021-04-21 21:05:41 -0600

    optimized

--- ml_benchmark_gen/script_plot_all_v2.sh
+++ ml_benchmark_gen/script_plot_all_v2.sh
@@ -1,8 +1,13 @@
 make clean
-for dsp in {96..12288..96}
+for dsp in {96..9216..96}
 do 
-    make run_script_v2  dsp=${dsp}  n_cores=4  area_ratio_mode=LP
+    make run_script  	dsp=${dsp}  	n_cores=8  		area_ratio_mode=LP
 	make clean
+done
+
+for dsp in {96..9216..96}
+do 
+    make plot_speedup_per_benchmarks exp_dir=./results/DSP${dsp} area_ratio_effect=None
 done
 
 python3 plot_speedup_per_NumOfDSPs.py
